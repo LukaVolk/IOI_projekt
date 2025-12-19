8305127f4bd013b93335d9331e6a9c8c4b998fac
Iztok Bajcar <iztok.bajcar@gmail.com>
2025-12-19 12:07:24 +0100

    Added PM10 conventration visualization to map

--- vis.py
+++ vis.py
@@ -29,22 +29,60 @@ def plot_pm10_concentration(df):
     plt.show()
 
 
-def plot_map():
+def plot_map(df, timestamp):
     slovenia_gdf = gpd.read_file(SLOVENIA_GEOJSON_URL)
     cities_df = load_cities_data()
+
+    filtered_data = df[df["timestamp"] == timestamp]
+    cities = cities_df["city"].tolist()
+
+    x = cities_df["longitude"]
+    y = cities_df["latitude"]
+
+    # extract pm10 values for each city in cities
+    pm10 = []
+    for city in cities:
+        pm10_value = filtered_data[filtered_data["city"] == city]["PM10"].values
+        if len(pm10_value) > 0:
+            pm10.append(pm10_value[0])
+        else:
+            pm10.append(0)
+
+    sizes = [value * 2 for value in pm10]
+
+    colormap = plt.cm.plasma
+    norm = plt.Normalize(vmin=min(pm10), vmax=max(pm10))
+    colors = [colormap(norm(value)) for value in pm10]
+
+    # create the color scale bar
+    color_scale = plt.cm.ScalarMappable(norm=norm, cmap=colormap)
 
     fig, ax = plt.subplots(figsize=(10, 10))
     slovenia_gdf.plot(ax=ax, color="lightgrey", edgecolor="white")
     ax.scatter(
-        cities_df["longitude"],
-        cities_df["latitude"],
-        s=50,
-        color="red",
+        x=x,
+        y=y,
+        s=sizes,
+        color=colors,
         alpha=0.6,
     )
+
+    # add text labels for each city
+    for i, city in enumerate(cities):
+        ax.text(
+            x[i],
+            y[i] + 0.01,
+            pm10[i],
+            fontsize=10,
+            ha="center",
+            va="bottom",
+        )
+
+    ax.figure.colorbar(color_scale, ax=ax, label="PM10 Concentration")
     ax.set_title("Map")
     plt.show()
 
 
 if __name__ == "__main__":
-    plot_map()
+    combined_data = load_combined_data()
+    plot_map(combined_data, "2024-02-03 11:50:00")
